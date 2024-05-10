cba4b3970193ecbcef6629e7039552fcdfd458fb
Arslan Ablikim <arslan.ablikim0@gmail.com>
2024-05-10 12:34:32 +0800

    implemented SIWE

--- src/components/ConnectButton/index.tsx
+++ src/components/ConnectButton/index.tsx
@@ -1,26 +1,44 @@
 "use client"
-
-import { useAccount, useConnect, useDisconnect } from "wagmi";
+import Image from "next/image";
+import { useAccount, useConnect, useDisconnect, useEnsAvatar, useEnsName } from "wagmi";
 import { useIsMounted } from "@/utils/usIsMounted";
+import { SignInButton } from "@/components/SignInButton";
 
 export default function ConnectButton() {
-  const { isConnected } = useAccount();
-  const { connect, connectors, error } = useConnect();
-  const { disconnect } = useDisconnect();
   const isMounted = useIsMounted()
+  const { address, connector, isConnected } = useAccount()
+  const { data: ensName } = useEnsName({ address })
+  const { data: ensAvatar } = useEnsAvatar({ name: ensName ?? undefined })
+  const { connect, connectors, error } =
+    useConnect()
+  const { disconnect } = useDisconnect()
 
-  if (!isMounted) return null
+  if (!isMounted) return null;
 
-  return isConnected ? (
-    <button onClick={() => disconnect()}>Disconnect Wallet</button>
-  ) : (
+  if (isConnected) {
+    return (
+      <div>
+        {ensAvatar && <Image src={ensAvatar} alt="ENS Avatar"/>}
+        <div>{ensName ? `${ensName} (${address})` : address}</div>
+        <div>Connected to {connector?.name}</div>
+        <button onClick={() => disconnect()}>Disconnect Wallet</button>
+        <SignInButton/>
+      </div>
+    )
+  }
+
+  return (
     <div>
       {connectors.map((connector) => (
-        <button key={connector.id} onClick={() => connect({ connector })}>
-          Connect with {connector.name}
+        <button
+          key={connector.id}
+          onClick={() => connect({ connector })}
+        >
+          {connector.name}
         </button>
       ))}
-      {error && <p>{error.message}</p>}
+
+      {error && <div>{error.message}</div>}
     </div>
-  );
+  )
 }
---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,51 @@
+import { getNounce } from "@/app/actions/getNounce";
+import { useState, useTransition } from "react";
+import { SiweMessage } from "siwe";
+import { useAccount, useSignMessage } from "wagmi";
+import { verifyNonce } from "@/app/actions/verifyNonce";
+
+export function SignInButton() {
+  const [isPending, startTransition] = useTransition();
+  const { address, chainId } = useAccount();
+  const { signMessageAsync } = useSignMessage();
+  const [error, setError] = useState<string>();
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
+  const handleSignIn = async () => {
+    startTransition(async () => {
+      if (!address || !chainId) {
+        setError('Connect to a wallet first.');
+        return;
+      }
+      const nonce = await getNounce();
+      // Create SIWE message with pre-fetched nonce and sign with wallet
+      const message = new SiweMessage({
+        domain: window.location.host,
+        address,
+        statement: 'Sign in with Ethereum to the app.',
+        uri: window.location.origin,
+        version: '1',
+        chainId,
+        nonce,
+      });
+      const signature = await signMessageAsync({
+        message: message.prepareMessage(),
+      });
+      const response = await verifyNonce(JSON.stringify(message), signature);
+      console.log(response, message);
+      if (response.ok) {
+        setIsSignedIn(true);
+      } else {
+        setError(response.error);
+      }
+    });
+  }
+  if (isSignedIn) {
+    return <div>Successfully signed in!</div>;
+  }
+  return (
+    <div>
+      <button onClick={handleSignIn} disabled={isPending}>{isPending ? 'logging in ...' : 'Sign In With SIWE'}</button>
+      {error && <div>{error}</div>}
+    </div>
+  );
+}
